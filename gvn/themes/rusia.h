7d46a7f11610636483135391676c8f47bef00451
g3nkzn <fabian.huamani@estudiante.ucsm.edu.pe>
2025-05-31 18:30:35 +0000

    Actualizacion

--- gvn/themes/rusia.h
+++ gvn/themes/rusia.h
@@ -1,8 +1,8 @@
 static const char black[]     = "#0e0e0e";      // Negro puro
 static const char white[]     = "#c0c0c0";     // Gris claro (casi blanco)
-static const char gray2[]     = "#1c1c1c";     // Gris muy oscuro (borde ventana inactiva)
-static const char gray3[]     = "#3a3a3a";     // Gris oscuro
-static const char gray4[]     = "#e0e0e0";     // Gris muy claro
+static const char gray2[]     = "#0e0e0e";     // Gris muy oscuro (borde ventana inactiva)
+static const char gray3[]     = "#484847";     // Gris oscuro
+static const char gray4[]     = "#0e0e0e";     // Gris muy claro
 static const char red[]       = "#ff0000";     // Rojo puro (borde ventana activa)
 static const char dark_red[]  = "#5e0e0e";     // Rojo oscuro (antes verde)
 static const char red2[]      = "#ff3300";     // Rojo anaranjado
